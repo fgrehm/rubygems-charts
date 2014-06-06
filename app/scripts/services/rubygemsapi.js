972b73dd147c3aef24a683a4247337a821489415
Fabio Rehm <fgrehm@gmail.com>
2014-06-06 15:55:23 -0300

    Implement the RubyGemsApi service

--- app/scripts/services/rubygemsapi.js
+++ app/scripts/services/rubygemsapi.js
@@ -1,6 +1,16 @@
 'use strict';
 
 angular.module('rubygemsChartsApp')
-  .service('Rubygemsapi', function Rubygemsapi() {
-    // AngularJS will instantiate a singleton by calling "new" on this function
+  .factory('RubyGemsApi', function($http) {
+    var rgApi = {};
+
+    rgApi.fetchDownloadStats = function(gemName) {
+      var url = 'http://rubygems-jsonp.herokuapp.com/versions/' + gemName + '?callback=JSON_CALLBACK';
+      return $http({
+        method: 'JSONP',
+        url:    url
+      });
+    };
+
+    return rgApi;
   });
