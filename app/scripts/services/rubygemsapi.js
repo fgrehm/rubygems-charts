dce188ddac85c2c5c90e8db0161b78d19603301c
Fabio Rehm <fgrehm@gmail.com>
2014-06-06 15:54:44 -0300

    yo angular:service RubyGemsApi

---
+++ app/scripts/services/rubygemsapi.js
@@ -0,0 +1,6 @@
+'use strict';
+
+angular.module('rubygemsChartsApp')
+  .service('Rubygemsapi', function Rubygemsapi() {
+    // AngularJS will instantiate a singleton by calling "new" on this function
+  });
