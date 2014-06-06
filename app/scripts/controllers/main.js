e710805ab25e88995abd1156cba52de6fbcbf516
Fabio Rehm <fgrehm@gmail.com>
2014-06-06 17:33:44 -0300

    Hook up controller with API data

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -1,9 +1,9 @@
 'use strict';
 
 angular.module('rubygemsChartsApp')
-  .controller('MainCtrl', function ($scope) {
-    $scope.rubyGemVersions = [
-      { number: "1.0.0", downloads_count: 123 },
-      { number: "2.0.0", downloads_count: 456 }
-    ]
+  .controller('MainCtrl', function ($scope, RubyGemsApi) {
+    $scope.rubyGemVersions = [];
+    RubyGemsApi.fetchDownloadStats('letter_opener').success(function(response) {
+      $scope.rubyGemVersions = response.data;
+    });
   });
