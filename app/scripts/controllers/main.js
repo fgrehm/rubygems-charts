41316e09ac944d160cecf91ecc12d70abc879159
Fabio Rehm <fgrehm@gmail.com>
2014-06-06 19:12:32 -0300

    Render data on charts

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -2,10 +2,26 @@
 
 angular.module('rubygemsChartsApp')
   .controller('MainCtrl', function ($scope, RubyGemsApi) {
-    $scope.rubyGemVersions = [];
     $scope.loadGemStats = function() {
       RubyGemsApi.fetchDownloadStats($scope.gemName).success(function(response) {
-        $scope.rubyGemVersions = response.data;
+        var rows = [];
+        for (var i = 0; i < response.data.length; i++) {
+          var version = response.data[i];
+          rows.unshift({
+            c: [ { v: version.number }, { v: version.downloads_count } ]
+          });
+        }
+
+        $scope.rubyGemVersionsChart = {
+          type: 'LineChart',
+          data: {
+            cols: [
+              {id: "t", label: "Versions",  type: "string"},
+              {id: "s", label: "Downloads", type: "number"}
+            ],
+            rows: rows
+          }
+        };
       });
     }
   });
